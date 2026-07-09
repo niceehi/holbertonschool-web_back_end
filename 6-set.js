4644e56b0f363297bb366d1d5d3d3ae531a4ca40
niceehi <isltur464536@gmail.com>
2026-07-09 16:30:27 +0400

    Delete

--- 6-set.js
+++
@@ -1,9 +0,0 @@
-export default function setFromArray(array) {
-  const mySet = new Set();
-  if (Array.isArray(array)) {
-    for (const item of array) {
-      mySet.add(item);
-    }
-  }
-  return mySet;
-}

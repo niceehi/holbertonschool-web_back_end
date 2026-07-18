390ea3e2aa002fb931de96312a2bb1a135135ee2
niceehi <isltur464536@gmail.com>
2026-07-18 17:22:20 +0400

    Task

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,14 +1,11 @@
 export default function handleResponseFromAPI(promise) {
-  const myPromise = new Promise((resolve, reject) => {
-    promise
-      .then(() => {
-        console.log('Got a response from the API');
-        resolve({ status: 200, body: 'Success' });
-      })
-      .catch(() => {
-        reject(new Error());
-      });
-  });
-
-  return myPromise;
+  return promise
+    .then(() => {
+      console.log('Got a response from the API');
+      return {
+        status: 200,
+        body: 'success',
+      };
+    })
+    .catch(() => new Error());
 }

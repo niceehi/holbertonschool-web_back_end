52fe31ec1f0621d5e952c4ebeaf414ee9cb173e3
niceehi <isltur464536@gmail.com>
2026-07-26 13:42:08 +0400

    Task

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,43 +1,38 @@
 const express = require('express');
 const countStudents = require('./3-read_file_async');
 
-const databasePath = process.argv[2];
-
 const app = express();
+const database = process.argv[2];
 
-// root route
 app.get('/', (req, res) => {
-  res.set('Content-Type', 'text/plain');
-  res.send('Hello Holberton School!');
+  res.status(200).type('text').send('Hello Holberton School!');
 });
 
-// /students route
-app.get('/students', async (req, res) => {
-  res.set('Content-Type', 'text/plain');
-  const text = 'This is the list of our students\n';
-
-  // save log to restore later
+app.get('/students', (req, res) => {
   const originalLog = console.log;
-
-  // empty string to catch logs
   let output = '';
 
-  // temporary log to catch log messages and build answer
-  console.log = (message) => { output += `${message}\n`; };
+  console.log = (msg) => {
+    output += `${msg}\n`;
+  };
 
-  // await the answer to avoid having asynchronous response
-  try {
-    await countStudents(databasePath);
-    console.log = originalLog;
-    res.send(text + output.trim());
-  } catch (err) {
-    // restore original log and show error message
-    console.log = originalLog;
-    res.send('Cannot load the database');
-  }
+  countStudents(database)
+    .then(() => {
+      console.log = originalLog;
+      res
+        .status(200)
+        .type('text')
+        .send(`This is the list of our students\n${output.trim()}`);
+    })
+    .catch(() => {
+      console.log = originalLog;
+      res
+        .status(200)
+        .type('text')
+        .send('This is the list of our students\nCannot load the database');
+    });
 });
 
-// listen on port 1245
 app.listen(1245);
 
 module.exports = app;
